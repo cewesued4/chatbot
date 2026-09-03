ac8995c784e14ea1011b1dd144b6abc0f5258a66
cewesued4 <cewesued@syr.edu>
2026-09-03 22:58:21 +0000

    Created a home page

---
+++ Home.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Home Page") #needed to add this extra page as I was getting
+#a recursion error. streamlit_app.py is not a page and I needed to make a separate home page.
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 
-Home = st.Page("streamlit_app.py", title="Home", default=True)
+st.set_page_config(page_title="Navigation Page")
+
+Home = st.Page("Home.py", title="Home", default=True)
 Lab1 = st.Page('Lab1.py', title = "Lab1")
 Lab2 = st.Page('Lab2.py', title = "Lab2")
-pg = st.navigation([Lab1, Lab2])
-st.set_page_config(page_title = 'Navigation Page')
+pg = st.navigation([Home, Lab1, Lab2])
+
 pg.run()
