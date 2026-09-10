5dd6bdd81236ff62c3b289d649995c4956cf15a0
cewesued4 <cewesued@syr.edu>
2026-09-10 22:44:51 +0000

    Changed tabs to lab names

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 st.set_page_config(page_title="Navigation Page")
 
 Home = st.Page("Home.py", title="Home")
-Lab1 = st.Page('Lab1.py', title = "TAB 1")
-Lab2 = st.Page('Lab2.py', title = "CHATBOT HERE")
-Lab3 = st.Page('Lab3.py', title = "CHATBOT NEW", default=True)
+Lab1 = st.Page('Lab1.py', title = "Lab1")
+Lab2 = st.Page('Lab2.py', title = "Lab2")
+Lab3 = st.Page('Lab3.py', title = "Lab3", default=True)
 pg = st.navigation([Home, Lab1, Lab2, Lab3])
 
 pg.run()
